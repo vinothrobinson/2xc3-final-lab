dbb5fb3fa349557207d975380629422af4d44dfe
vinothrobinson <vinothrobinson03@gmail.com>
2023-03-22 08:54:58 -0400

    Create Bellman-Ford Approximation

--- part1.py
+++ part1.py
@@ -28,3 +28,25 @@ def dijkstra_approx(G, source, k):
                 pred[neighbour] = current_node
                 relaxed[neighbour] += 1
     return dist
+
+def bellman_ford_approx(G, source, k):
+    relaxed = {}
+    pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {}  # Distance dictionary
+    nodes = list(G.adj.keys())
+
+    # Initialize distances
+    for node in nodes:
+        dist[node] = float("inf")
+        relaxed[node] = 0
+    dist[source] = 0
+
+    # Meat of the algorithm
+    for _ in range(G.number_of_nodes()):
+        for node in nodes:
+            for neighbour in G.adj[node]:
+                if dist[neighbour] > dist[node] + G.w(node, neighbour) and relaxed[neighbour] < k:
+                    dist[neighbour] = dist[node] + G.w(node, neighbour)
+                    pred[neighbour] = node
+                    relaxed[neighbour] += 1
+    return dist
