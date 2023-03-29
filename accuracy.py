a6f9efb6d896085068c0aa66fc79bb2cc7d45a7d
vinothrobinson <vinothrobinson03@gmail.com>
2023-03-29 09:10:51 -0400

    Edit bounds of functions

--- accuracy.py
+++ accuracy.py
@@ -7,10 +7,10 @@ import min_heap
 import matplotlib.pyplot as plot
 """
 
-def experiment4_dijkstra(graph_size, max_edge, max_k_value):
+def experiment4_dijkstra(graph_size, min_edge, max_edge, max_k_value):
     total_dist = {}
     # Use this graph for all of the approximations
-    G = f.create_random_complete_graph(graph_size, max_edge)
+    G = f.create_random_complete_graph(graph_size, min_edge, max_edge)
     total_dist[0] = f.dijkstra(G, 0)
 
     # Calculates the approximate distance based on k value
@@ -37,10 +37,10 @@ def experiment4_dijkstra(graph_size, max_edge, max_k_value):
 
     return total_dist
     
-def experiment4_bellman(graph_size, max_edge, max_k_value):
+def experiment4_bellman(graph_size, min_edge, max_edge, max_k_value):
     total_dist = {}
     # Use this graph for all of the approximations
-    G = f.create_random_complete_graph(graph_size, max_edge)
+    G = f.create_random_complete_graph(graph_size, min_edge, max_edge)
     total_dist[0] = f.bellman_ford(G, 0)
 
     # Calculates the approximate distance based on k value
@@ -67,7 +67,13 @@ def experiment4_bellman(graph_size, max_edge, max_k_value):
 
     return total_dist
 
-experiment4_bellman(50, 50, 49)
+# experiment4_dijkstra(10, 1, 10, 9)
+# experiment4_dijkstra(25, 1, 25, 24)
+# experiment4_dijkstra(50, 1, 50, 49)
+
+# experiment4_bellman(10, 1, 10, 9)
+# experiment4_bellman(25, 1, 25, 24)
+experiment4_bellman(50, 1, 50, 49)
 
 """
 Optional print statement removed to improve run time
