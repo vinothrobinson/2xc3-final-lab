063f3e89bf049b38845f56059cc753b9ffb15670
Karl <saderk@mcmaster.ca>
2023-04-05 10:52:40 -0400

    undirected

--- final_project_part1.py
+++ final_project_part1.py
@@ -32,6 +32,38 @@ class DirectedWeightedGraph:
     def number_of_nodes(self):
         return len(self.adj)
 
+class UnDirectedWeightedGraph:
+
+    def __init__(self):
+        self.adj = {}
+        self.weights = {}
+
+    def are_connected(self, node1, node2):
+        for neighbour in self.adj[node1]:
+            if neighbour == node2:
+                return True
+        return False
+
+    def adjacent_nodes(self, node):
+        return self.adj[node]
+
+    def add_node(self, node):
+        self.adj[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        if node2 not in self.adj[node1]:
+            self.adj[node1].append(node2)
+        if node1 not in self.adj[node2]:
+            self.adj[node2].append(node1)
+        self.weights[(node1, node2)] = weight
+
+    def w(self, node1, node2):
+        if self.are_connected(node1, node2):
+            return self.weights[(node1, node2)]
+
+    def number_of_nodes(self):
+        return len(self.adj)
+
 
 def dijkstra(G, source):
     pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
