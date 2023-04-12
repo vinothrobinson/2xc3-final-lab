4e1f41685ff1a7867e63a2c01e84421c84f50319
--global <brandonb@wightman.ca>
2023-04-12 10:14:45 -0400

    added getting transfer data for experiment suite 2

--- experiment2.py
+++ experiment2.py
@@ -1,7 +1,17 @@
-# def experiment5(subway):
-#     line_data = [] #TODO
-#
-#     for line1 in
-#
-# subway_graph = karl.csv_graph()
-# line_data = []
+import csv
+
+import karl
+
+
+def experiment5(subway, transfer_data):
+    pass
+
+subway_graph = karl.csv_graph()
+transfer_data = {}
+with open('karl1.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        transfer_data[(row['station1'], row['station2'])] = row['trasfers']
+        transfer_data[(row['station2'], row['station1'])] = row['transfers']
+
+experiment5(subway_graph, transfer_data)
