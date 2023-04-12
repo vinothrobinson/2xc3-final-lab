0eb8840a8b80c0b64fa2e8237f30a10bbbfca896
--global <brandonb@wightman.ca>
2023-04-12 10:18:37 -0400

    Put getting transfer data into a function

--- experiment2.py
+++ experiment2.py
@@ -1,17 +1,26 @@
 import csv
+import timeit
 
 import karl
 
 
 def experiment5(subway, transfer_data):
-    pass
+    for line1 in subway.adj.keys():
+        for line2 in subway.adj.keys():
+            if transfer_data[(line1, line2)] == 0:
+                start = timeit.default_timer()
+
+
+def get_transfer_data():
+    transfer_data = {}
+    with open('karl1.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            transfer_data[(row['station1'], row['station2'])] = row['transfers']
+            transfer_data[(row['station2'], row['station1'])] = row['transfers']
+    return transfer_data
+
 
 subway_graph = karl.csv_graph()
-transfer_data = {}
-with open('karl1.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        transfer_data[(row['station1'], row['station2'])] = row['trasfers']
-        transfer_data[(row['station2'], row['station1'])] = row['transfers']
-
+transfer_data = get_transfer_data()
 experiment5(subway_graph, transfer_data)
